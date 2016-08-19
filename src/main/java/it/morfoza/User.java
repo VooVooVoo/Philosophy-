2f658397f67aa808c1f57f8d60d4b409e9afb854
Wojt <ciechman.ws@gmail.com>
2016-08-19 17:06:17 +0200

    Adding start page

---
+++ src/main/java/it/morfoza/User.java
@@ -0,0 +1,17 @@
+package it.morfoza;
+
+/**
+ * Created by Wojt on 2016-08-19.
+ */
+public class User {
+    String nick;
+    Philosopher philosopher;
+    int level;
+
+    User user = new User();
+    this.nick = userNick;
+    this.philosopher = userPhilosopher;
+    this.level = userLevel;
+
+
+}
